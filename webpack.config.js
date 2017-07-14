7abc42a2c266383e0cc9fb408f0487df016edac7
Vadim Merkotan <persidskij_on_ferrari@ukr.net>
2017-07-14 14:54:21 +0300

    https://www.robinwieruch.de/minimal-react-webpack-babel-setup/

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const path = require('path');
- 
 module.exports = {
-  context: path.join(__dirname, 'src'),
   entry: [
-    './main.js',
+    'webpack-dev-server/client?http://localhost:8081',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
   ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-    ],
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    }]
   },
   resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
+    extensions: ['*', '.js', '.jsx']
   },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 };
