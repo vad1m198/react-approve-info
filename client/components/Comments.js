52b8cb7949870b62a861475cd9d7e8dfc5a08807
Vadim Merkotan <persidskij_on_ferrari@ukr.net>
2017-07-14 12:12:22 +0300

    finish app

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ const Comments = React.createClass({
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className="remove-comment">&times;</button>
+                    <button onClick={this.props.removeComment.bind(null, this.props.params.postId, i)} className="remove-comment">&times;</button>
                 </p>
             </div>
         )
@@ -16,13 +16,22 @@ const Comments = React.createClass({
         return (
             <div className = "comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form">
-                    <input type="text" ref="author" placeholder="autor" />
+                <form ref="commentForm" className="comment-form"
+                        onSubmit={this.handleSubmit}>
+                    <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
                 </form>
             </div>
         )
+    },
+    handleSubmit(e){
+        e.preventDefault();
+        const { postId } = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();
     }
 })
 
