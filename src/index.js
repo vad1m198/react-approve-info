daf589f40cd8be93465289b492a6a870dba13ac1
Vadim Merkotan <persidskij_on_ferrari@ukr.net>
2017-07-14 15:26:52 +0300

    simple app with sf icons

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const title = 'My Minimal React Webpack Babel Setup';
-
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
-
-module.hot.accept();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
