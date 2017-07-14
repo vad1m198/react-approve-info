414514f0dc69ceb8c2131b399d9b6dfad7942d0b
vadim merkotan <vadim.merkotan@silverlinecrm.com>
2017-07-14 08:53:58 +0300

    video #14

--- client/store.js
+++ client/store.js
@@ -1,15 +1,17 @@
-import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
 import { browserHistory } from 'react-router';
-//import the root reducer
+
+// import the root reducer
 import rootReducer from './reducers/index';
 
 import comments from './data/comments';
 import posts from './data/posts';
 
-//create an object for the default data
+// create an object for the default data
 const defaultState = {
-    posts,comments
+  posts,
+  comments
 };
 
 const store = createStore(rootReducer, defaultState);
